17cf30c8f695ddaa69a7215656d9d55581f0395e
samuelhtb <samuelbatubara05@gmail.com>
2026-05-04 09:40:43 +0700

    feat: implement wire routes in main

--- foodcourt-backend/cmd/api/main.go
+++ foodcourt-backend/cmd/api/main.go
@@ -5,23 +5,53 @@ import (
 	"os"
 
 	"github.com/gin-gonic/gin"
-	
-	"github.com/samuelhtb/food-court-system/foodcourt-backend/internal/config" 
+	"github.com/joho/godotenv"
+
+	"github.com/samuelhtb/food-court-system/foodcourt-backend/internal/config"
+	"github.com/samuelhtb/food-court-system/foodcourt-backend/internal/handlers"
+	"github.com/samuelhtb/food-court-system/foodcourt-backend/internal/repositories"
+	"github.com/samuelhtb/food-court-system/foodcourt-backend/internal/services"
 )
 
 func main() {
-	config.ConnectDB()
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Peringatan: File .env tidak ditemukan, menggunakan environment default")
+	}
+
+	secretKey := os.Getenv("JWT_SECRET")
+	if secretKey == "" {
+		log.Fatal("Error: JWT_SECRET belum diatur di file .env")
+	}
+
+	// db connection
+	db := config.ConnectDB()
+
+	// dependency injection
+	userRepo := repositories.NewUserRepository(db)
+	userService := services.NewUserService(userRepo)
+	userHandler := handlers.NewUserHandler(userService, secretKey)
+
 	r := gin.Default()
 
+	// route ping for connection test
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Connected successfully!",
 		})
 	})
 
+	// route api
+	api := r.Group("/api/v1")
+	{
+		api.POST("/register", userHandler.Register)
+		api.POST("/login", userHandler.Login)
+	}
+
+	// port configuration
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080" 
+		port = "8080"
 	}
 
 	log.Printf("Server berjalan di port %s", port)
